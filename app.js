de7164cc7669b08a3966dd1edeb04c7e061650c5
xyushi <ayuship03@gmail.com>
2022-07-26 16:43:52 +0530

    Commit Message

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 //Enter your code here..
+const btn = document.getElementById("btnGet")
+btn.onclick = function() {pullData()};
+
+async function pullData() {
+    try {
+        let response = await fetch("https://jsonplaceholder.typicode.com/users")
+        let data = await response.json()
+        document.querySelector("#message").innerHTML = "<h2>List of Users</h2>"
+        data.map((ele,index) =>{
+            document.querySelector('#message').innerHTML += `<div class='player' id='box${index}'></div>`
+            document.querySelector("#box"+index).innerHTML += `<p class='strength'>Name: ${ele.name}</p>`
+            document.querySelector("#box"+index).innerHTML += `<p>Email: ${ele.email}</p>`
+            document.querySelector("#box"+index).innerHTML += `<p>Phone: ${ele.phone}</p>`
+            document.querySelector("#box"+index).innerHTML += `<p>Website: ${ele.website}</p>`
+            document.querySelector("#box"+index).innerHTML += `<p>Company: ${ele.company.name}</p>`
+            document.querySelector("#box"+index).innerHTML += `<p>City: ${ele.address.city}</p>`
+            document.querySelector("#box"+index).innerHTML += `<p>Zipcode: ${ele.address.zipcode}</p>`
+        })
+    }
+    catch (error) {
+        console.log('Promise rejected');
+        console.log(error);
+    }
+}
